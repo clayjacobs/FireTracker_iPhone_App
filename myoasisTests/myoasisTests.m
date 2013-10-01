3c11629a130ae2ad73dc549a331fae86e319d22b
Andrew Huynh <a5.t.huynh@gmail.com>
2013-10-01 09:27:36 -0700

    Initial Commit

---
+++ myoasisTests/myoasisTests.m
@@ -0,0 +1,34 @@
+//
+//  myoasisTests.m
+//  myoasisTests
+//
+//  Created by Andrew on 10/1/13.
+//  Copyright (c) 2013 DHLabs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface myoasisTests : XCTestCase
+
+@end
+
+@implementation myoasisTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
